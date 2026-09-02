aeb7bf37239893d85e84890e165d5737c75a9536
Hoà <253856784+NDCLI@users.noreply.github.com>
2026-09-02 23:01:00 +0700

    chore: bump version to 1.0.16

--- installer/Build-Setup.ps1
+++ installer/Build-Setup.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param(
     [ValidatePattern('^\d+\.\d+\.\d+$')]
-    [string]$Version = '1.0.15',
+    [string]$Version = '1.0.16',
     [string]$CertificateThumbprint,
     [string]$PfxPath,
     [string]$TimestampUrl = 'http://timestamp.digicert.com'
